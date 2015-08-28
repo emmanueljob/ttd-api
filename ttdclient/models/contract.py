bbce9917166a2c87fb113be992526553e8509647
Emmanuel Job <emmanuel.job@accuenmedia.com>
2015-08-28 20:57:08 +0000

    add function to get contracts by adv

--- ttdclient/models/contract.py
+++ ttdclient/models/contract.py
@@ -17,6 +17,17 @@ class Contract(Base):
         response = self._execute(method, url, json.dumps(payload))
         return self._get_response_objects(response)
 
+    def find_by_advertiser(self, partnerId, advertiserId):
+        payload = { "PartnerId": partnerId,
+                    "AdvertiserId": advertiserId,
+                    "PageStartIndex": 0,
+                    "PageSize": None }
+        method = "POST"
+        url = '{0}/{1}'.format(self.get_url(), 'query/advertiser/available')
+        
+        response = self._execute(method, url, json.dumps(payload))
+        return self._get_response_objects(response)
+
     def get_avails(self, contractId, startDate, endDate):
         payload = { "ContractId": contractId,
                     "ReportStartDateUtc": startDate,
