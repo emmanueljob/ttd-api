6bb8350346eb9ec9b4e48722e4c2370d5b059803
Matt Artingstall <matt@imagndesign.com>
2016-01-06 22:58:50 +0000

    Get creatives based on advertiser ID.

--- ttdclient/models/creative.py
+++ ttdclient/models/creative.py
@@ -6,6 +6,16 @@ from ttdclient.models.base import Base
 class Creative(Base):
 
     obj_name = "creative"
+
+    def get_by_advertiser(self, advertiser_id):
+        payload = { "AdvertiserId": advertiser_id,
+                    "PageStartIndex": 0,
+                    "PageSize": None }
+        method = "POST"
+        url = '{0}/{1}'.format(self.get_url(), 'query/advertiser')
+        
+        response = self._execute(method, url, json.dumps(payload))
+        return self._get_response_objects(response)
     
     def get_size(self):
         
