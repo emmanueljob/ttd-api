f10e0b4a16b5c3bfee21f44cd00ad8e12894e79c
Matt Artingstall <matt@imagndesign.com>
2017-02-15 11:13:13 -0800

    Param for returning only contract IDs

--- ttdclient/models/contract.py
+++ ttdclient/models/contract.py
@@ -17,11 +17,13 @@ class Contract(Base):
         response = self._execute(method, url, json.dumps(payload))
         return self._get_response_objects(response)
 
-    def find_by_advertiser(self, partnerId, advertiserId):
+    def find_by_advertiser(self, partnerId, advertiserId, returnOnlyIds=False):
         payload = { "PartnerId": partnerId,
                     "AdvertiserId": advertiserId,
                     "PageStartIndex": 0,
                     "PageSize": None }
+        if returnOnlyIds:
+            payload['ReturnOnlyIds'] = "true"
         method = "POST"
         url = '{0}/{1}'.format(self.get_url(), 'query/advertiser/available')
         
