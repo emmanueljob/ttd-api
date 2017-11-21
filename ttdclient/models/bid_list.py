14cba43d1dd8414011ea2df0345a12a20cac8ade
Matt Artingstall <matt@imagndesign.com>
2017-11-21 08:31:17 -0800

    Missed obj_name

--- ttdclient/models/bid_list.py
+++ ttdclient/models/bid_list.py
@@ -4,15 +4,17 @@ from ttdclient.models.base import Base
 
 class BidList(Base):
 
+    obj_name = "bidlist"
+
     def getId(self):
         return self.get("BidListId")
 
     def create_bid_list(self, name, target_list='TargetList', resolution_type='SingleMatchOnly'):
         self['BidList'] = { 
-            'Name' = name,
-            'BidListAdjustmentType' = target_list,
-            'ResolutionType' = resolution_type,
-            'BidLines' = []
+            "Name": name,
+            "BidListAdjustmentType": target_list,
+            "ResolutionType": resolution_type,
+            "BidLines": []
             }
 
     def set_bid_lines(self, lines):
