abdfb774bd2ffbe8f5b71f0c27fd1821accc711f
vagrant <vagrant@vagrant.vm>
2017-12-05 22:14:54 +0000

    If found, use domain's current bid adjustment

--- ttdclient/models/site_list.py
+++ ttdclient/models/site_list.py
@@ -39,12 +39,12 @@ class SiteList(Base):
         print hits
         print "END PRINTING SELF"
         for domain in list(set(domains)):
-            default_adjustment = 1.0
+            the_adjustment = 1.0
             print "Searching for domain:"
             print domain
             if hits.has_key(domain):
                 print "Domain found in dictionary"
-                default_adjustment = hits[domain]
-            to_add.append({'Domain': domain, 'adjustment': 1.0})
+                the_adjustment = hits[domain]
+            to_add.append({'Domain': domain, 'adjustment': the_adjustment})
         self['SiteListLines'] = to_add
 
